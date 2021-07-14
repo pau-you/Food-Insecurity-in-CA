213115cdc7ca86d349ea806484db3585a6671f3d
jeffry-zheng <jzheng1@berkeley.edu>
2021-07-13 17:16:33 -0700

    more pages

---
+++ streamlit_app/pages/about.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import datetime
+import pandas as pd
+import numpy as np
+
+def app():
+
+    st.title("About Secure the Bag")
+    st.write("This is our MIDS Capstone Project!")
---
+++ streamlit_app/pages/resources.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import datetime
+import pandas as pd
+import numpy as np
+
+def app():
+
+    st.title("Resources")
+    st.write("compiled resources")
