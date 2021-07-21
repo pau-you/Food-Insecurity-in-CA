0ca8daf886805830910da128f65cfcb1c46bc1b1
jeffry-zheng <jzheng1@berkeley.edu>
2021-07-20 21:53:26 -0700

    file select

--- streamlit_app/pages/home.py
+++ streamlit_app/pages/home.py
@@ -6,7 +6,8 @@ import numpy as np
 from PIL import Image
 
 def app():
-    st.write(os.getcwd())
+    path = os.path.dirname(__file__)
+    st.write(path)
     dining = Image.open('./photos/pexels-august-de-richelieu-4262173.jpg')
     st.image(dining)
     st.markdown('_“In this country that grows more food than any other nation on this earth, it is unthinkable that any child should go hungry.”  - Sela Ward_')
